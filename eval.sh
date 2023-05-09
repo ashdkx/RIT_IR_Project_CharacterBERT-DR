3b15ab723ab99748720f49ac7eefd5fbfd504f8a
mdv7374 <54516581+mdv7374@users.noreply.github.com>
2023-05-08 20:15:24 -0400

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#Bash script used to retrieve results from retrieval task
 eval_files=("character_bert_st_dltypo_typo_rank3.txt.trec" "character_bert_st_dltypo_typo_rank4.txt.trec" "character_bert_st_dltypo_typo_rank5.txt.trec" "character_bert_st_dltypo_typo_rank6.txt.trec" "character_bert_st_dltypo_typo_rank7.txt.trec" "character_bert_st_dltypo_typo_rank8.txt.trec")
 
 for i in ${!eval_files[@]}; do
+#runs eval for MAP, ndcg, and recip_rank
 trec_eval -l 2 -m ndcg_cut.10 -m map -m recip_rank data/dl-typo/qrels.txt eval_files/${eval_files[$i]}
 
 done  
